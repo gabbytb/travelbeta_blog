43a5abe3b2c3deb9cb483c786cc041fa2207405c
Gabby <igabrieloyebanji@protonmail.com>
2025-02-06 09:12:15 +0100

    Ready to Deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = Geist({
@@ -35,7 +34,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 {children}
-                <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
