56c6c220ac767c21aa4afc44ebe7a85bec6b1ed6
Gabby <igabrieloyebanji@protonmail.com>
2025-02-07 20:46:49 +0100

    Ready to Deploy

--- src/utils/setToNGTime.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-    const dateTokenExpires = new Date();
-    // Add 1 hour (60 minutes * 60 seconds * 1000 milliseconds)
-    dateTokenExpires.setHours(dateTokenExpires.getHours() + 1);
-    // Format the date to ISO 8601 string
-    return dateTokenExpires.toISOString();
-};
