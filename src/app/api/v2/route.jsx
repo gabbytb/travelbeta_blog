e1a56231d2cab7bc075894859e760b44bef617ea
Gabby <igabrieloyebanji@protonmail.com>
2025-02-07 19:00:03 +0100

    Ready to Deploy

--- src/app/api/v2/route.jsx
+++ src/app/api/v2/route.jsx
@@ -9,7 +9,24 @@ import { USER_ENDPOINTS } from "@/app/endpoints";
 
 
 // ALL AXIOS FOR USERS MODEL
-export async function POST(requestData) {    
+// export async function POST(requestData) {    
+    
+//     try {
+                        
+//         // Send the data to the Express server
+//         const response = await axios.post(USER_ENDPOINTS.CREATE_ACCOUNT, requestData);
+//         return response.data;
+
+//     } catch (error) {
+//         console.error("Error creating post:", error);
+//         throw error;
+//     };  
+// };
+
+
+
+
+export const POST = async (requestData) => {
     
     try {
                         
@@ -20,6 +37,5 @@ export async function POST(requestData) {
     } catch (error) {
         console.error("Error creating post:", error);
         throw error;
-    };  
+    }; 
 };
-
