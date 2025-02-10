e28c0c05e9007baf5026dafe250f32fd02c73479
Gabby <igabrieloyebanji@protonmail.com>
2025-02-10 10:17:30 +0100

    Ready to Deploy

--- src/app/api/v1/route.jsx
+++ src/app/api/v1/route.jsx
@@ -1,4 +1,6 @@
+import { NextResponse } from "next/server";
 import axios from "axios";
+import { USER_ENDPOINTS } from "@/app/endpoints";
 
 
 
@@ -34,3 +36,35 @@ export async function POST(request) {
         return Response.json({ message: "Failed to create post", error }, { status: 500 });
     };
 };
+
+
+
+// CREATE NEW ACCOUNT
+export const createNewUser = async (request) => {
+    
+    try {
+        // Parse the request body from Next.js frontend
+        const requestData = await request.json();
+
+        // Send the data to the Express server
+        const response = await axios.post(USER_ENDPOINTS.CREATE_ACCOUNT, requestData);
+        
+        // ✅ Correctly return a Next.js response
+        return NextResponse.json(response.data, { status: response.status });
+
+    } catch (error) {
+        console.error("Error creating post:", error);
+
+        // ✅ Handle errors properly
+        return NextResponse.json(
+            { 
+                success: false, 
+                message: error.response?.data || "Internal Server Error" 
+            },
+            {  
+                status: error.response?.status || 500 
+            }
+        );
+    };
+};
+// export async function POST(requestData) {    
