3ef2f8b6343c97e984069015057bf2494afdfa8a
Gabby <igabrieloyebanji@protonmail.com>
2025-02-11 14:54:36 +0100

    Ready to Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,29 @@
+// TYPESCRIPT VERSION
 import type { NextConfig } from "next";
-
-
-
 const nextConfig: NextConfig = {
-    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com", // Replace with the actual domain
+            },
+        ],
+    },
 };
-
-
 export default nextConfig;
+
+
+
+// JAVASCRIPT VERSION
+// const nextConfig = {
+//     /* config options here */
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: "res.cloudinary.com", // Replace with the actual domain
+//             },
+//         ],
+//     },
+// };
+// module.exports = nextConfig;
